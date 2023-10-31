9f4eec8c023f053b1669b12cc7cfe351cb912373
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 05:31:30 -0700

    Adding file 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for alpha in range(122, 96, -1):
+    if alpha % 2 == 0:
+        print("{}".format(chr(alpha)), end="")
+    else:
+        print("{}".format(chr(alpha - 32)), end="")

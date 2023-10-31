b5a18be9285ff626d652493460a72ef59fcbc5df
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 04:25:00 -0700

    Adding file 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def isupper(str):
     for c in str:
-        if ord(str[i]) >= 65 and ord(str[i]) <= 90:
-            continue
+        if ord(c) >= 65 and ord(c) <= 90:
+            print("{}".format(c), end="")
         else:
             print("{:c}".format(ord(c) - 32), end="")
     print("\n")

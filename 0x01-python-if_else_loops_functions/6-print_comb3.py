d389b0a3aef878a3211428666c141dab89943108
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 03:30:52 -0700

    Adding file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
-for number in range(0, 100):
-    if number != 99:
-        print(f"{number:02d}, ", end="")
-    else:
-        print(99)
+for num1 in range(0, 10):
+    for num2 in range (0, 10):
+        if num1 == num2 or num2 < num1:
+            continue
+        elif num1 != 8 or num2 != 9:
+            print("{}{}, ".format(num1, num2).format(num1, num2), end="")
+        else:
+            print(89)

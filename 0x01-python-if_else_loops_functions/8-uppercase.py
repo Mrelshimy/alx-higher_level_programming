c4c3850b2debbf9a0728a0ca274d71f351eb648c
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 06:32:20 -0700

    Updating file 8

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,5 +2,5 @@
 def uppercase(str):
     for c in str:
         print("{:c}".format(ord(c)
-              if ord(c) >= 65 and ord(c) <= 90 else ord(c) - 32))
+              if ord(c) >= 65 and ord(c) <= 90 else ord(c) - 32), end="")
     print("")

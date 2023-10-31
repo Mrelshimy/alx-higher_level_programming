6f673e6519e56a51ce85bf6a09c127945074efbb
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 03:31:41 -0700

    Adding file 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num1 in range(0, 10):
-    for num2 in range (0, 10):
+    for num2 in range(0, 10):
         if num1 == num2 or num2 < num1:
             continue
         elif num1 != 8 or num2 != 9:

b2bd174a8bc15371a0f6cd693177b17d6a70207b
Mrelshimy <mraafat.elsayed@gmail.com>
2023-10-31 05:40:56 -0700

    Adding file 101

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    i = 0
+    while str[i]
+        if i == n:
+            continue
+        else:
+            print("{}".format(str[i]), end="")
+        i++

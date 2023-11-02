cd3e0d98317c193c3e9f54144341775d9d492ddf
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-02 12:09:58 -0700

    Updating file 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,8 +5,9 @@ if __name__ == "__main__":
     if nargs == 0:
         print("0 arguments.")
     elif nargs == 1:
-        print("1 argumnent:")
+        print("1 argument:")
+        print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(nargs))
-    for i in range(nargs):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+        for i in range(nargs):
+            print("{}: {}".format(i + 1, sys.argv[i + 1]))

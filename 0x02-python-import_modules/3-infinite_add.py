4bc950a387fb941a2f6e1dead56c39ca43da2975
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-02 06:07:05 -0700

    Adding file 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     total = 0
     nargs = len(sys.argv) - 1
     for i in range(nargs):
-       total += int(sys.argv[i + 1])
+        total += int(sys.argv[i + 1])
     print(total)

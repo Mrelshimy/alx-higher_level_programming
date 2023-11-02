9f111361fc59629cc2a9ea67cf5f393ac003f509
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-02 11:19:56 -0700

    updating file 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,26 +1,24 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-
     from calculator_1 import add, sub, mul, div
+
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exiti(1)
-    a = sys.argv[1]
-    oper = sys.argv[1]
-    b = sys.argv[2]
+        exit(1)
 
-    ops = ['+', '-', '*', '/']
-    for op in ops:
-        if oper != op:
-            print("Unknown operator. Available operators: +, -, * and /")
-            exit(1)
+    a = int(sys.argv[1])
+    oper = sys.argv[2]
+    b = int(sys.argv[3])
 
-    if oper == '+':
-        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
-    elif oper == '-':
-        print("{} {} {} = {}".format(a, oper, b, sub(a, b)))
-    elif oper == '*':
+    if oper == "*":
         print("{} {} {} = {}".format(a, oper, b, mul(a, b)))
-    else:
+    elif oper == "-":
+        print("{} {} {} = {}".format(a, oper, b, sub(a, b)))
+    elif oper == "+":
+        print("{} {} {} = {}".format(a, oper, b, add(a, b)))
+    elif oper == "/":
         print("{} {} {} = {}".format(a, oper, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)

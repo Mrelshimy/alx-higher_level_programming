62b18befe33112235a3d0e1bb470bc9b0d7aef09
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-02 06:05:47 -0700

    Adding file 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    total = 0
+    nargs = len(sys.argv) - 1
+    for i in range(nargs):
+       total += int(sys.argv[i + 1])
+    print(total)

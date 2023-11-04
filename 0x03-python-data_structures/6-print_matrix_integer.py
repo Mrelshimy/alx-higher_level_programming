d6e6d131a714304b216dffe4fd43edc068038e84
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-04 03:46:59 -0700

    Updating file 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,3 +1,7 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    
+    for sub in matrix:
+        if len(sub) == 0:
+            print(sub)
+        for i in range(len(sub)):
+            print("{}".format(sub[i]), end="\n" if i == len(sub) - 1 else " ")

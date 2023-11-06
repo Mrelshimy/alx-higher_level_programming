b61991f0ef44184a1e53f8feb26d2b0f4a7ea651
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-06 06:26:50 -0800

    Adding file 100

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,24 +1,24 @@
 #include <Python.h>
 
 /**
- *print_python_list_info - print Pyobject info
- *@p: Pylist Object
+ * print_python_list_info - print Pyobject info
+ * @p: Pylist Object
  */
-
 void print_python_list_info(PyObject *p)
 {
-	int obj_allocated, size, n;
+	int size, obj_allocated, n;
 	PyObject *object;
 
-	size = Py_SIZE(p);
-	obj_allocated = ((PylistObject *)p)->allocated;
+	size = PyList_Size(p);
+	obj_allocated = ((PyListObject *)p)->allocated;
+
 	printf("[*] Size of the Python List = %d\n", size);
 	printf("[*] Allocated = %d\n", obj_allocated);
 
 	for (n = 0; n < size; n++)
 	{
-		printf("Element %d", n);
 		object = PyList_GetItem(p, n);
-		printf("%s\n", PyTYPE(object)->tp_name);
+		printf("Element %d: %s\n", n, Py_TYPE(object)->tp_name);
 	}
 }
+

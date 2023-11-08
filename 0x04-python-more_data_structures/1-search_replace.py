1bbdd2348f7d45a43d194dc1f9ae06bcc95dda34
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-08 06:14:39 -0800

    updating file 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    my_list = list(map(lambda i: i if i != search else replace,
-                       my_list))
-    return(my_list)
+    return list(map(lambda i: i if i != search else replace, my_list))

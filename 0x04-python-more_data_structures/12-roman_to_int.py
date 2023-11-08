189a79330311741dfc7ecb55b1d064bb94fa5215
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-08 12:03:49 -0800

    Adding file 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    if roman_string is None:
+    if not isinstance(roman_string, str):
         return 0
     rom_alpha = {'I': 1, 'V': 5,
                  'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}

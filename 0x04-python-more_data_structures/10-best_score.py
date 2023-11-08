7a02373580c175bf78c9485fe74b0ea65bdc6041
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-08 05:06:49 -0800

    Adding file 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    max = 0
+    biggest = ""
+    for i in a_dictionary:
+        if a_dictionary[i] > max:
+            max = a_dictionary[i]
+            biggest = i
+    return biggest

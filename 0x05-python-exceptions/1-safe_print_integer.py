da6d7b0bba1bbce208ef58f1d888bca620d0e647
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-20 01:18:15 -0800

    updating file 1

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
-def safe_print_list(my_list=[], x=0):
-    i = 0
+def safe_print_integer(value):
     try:
-        for element in range (x):
-            print("{}".format(my_list[element]), end="")
-        print()
-        return x
-    except:
-        for element in my_list:
-            i += 1
-        print()
-        return i
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False

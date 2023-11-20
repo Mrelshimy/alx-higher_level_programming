6339e67319ccb1d89d19e60deb188951c21d5dd8
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-20 04:36:07 -0800

    updating file 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 def safe_print_integer_err(value):
+    import sys
     try:
         print("{:d}".format(value))
         return True
     except ValueError as ve:
-        print("Exception: {}".format(ve))
+        print("Exception: {}".format(ve), file = sys.stderr)
         return False
     except TypeError as te:
-        print("Exception: {}".format(te))
+        print("Exception: {}".format(te), file = sys.stderr)
         return False

9d109dda14e11f956254025662f6be340a931532
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-20 01:08:49 -0800

    Updating files

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,11 +2,11 @@
 def safe_print_list(my_list=[], x=0):
     i = 0
     try:
-        for element in range(x):
+        for element in range (x):
             print("{}".format(my_list[element]), end="")
         print()
         return x
-    except IndexError:
+    except:
         for element in my_list:
             i += 1
         print()

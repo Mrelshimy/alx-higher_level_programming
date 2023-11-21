26347bc7d279d4097652518503135627182cfaa9
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-21 02:51:30 -0800

    Adding file 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,14 @@
 #!/usr/bin/python3
+"""Creating Class that defines a square"""
+
+
 class Square:
-    """Square , empty class"""
+    """Square , class to define a square"""
+
     def __init__(self, size):
+        """Constructor
+
+        Args:
+            size: square side length
+        """
         self.__size = size

369d14026babbb69dfafbeb8ede4b437158e6830
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-24 06:30:33 -0800

    update

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -11,10 +11,17 @@ def add_integer(a, b=98):
 
         Raises:
             TypeError: Raise if variables are not int or float
+
+        Returns:
+            Int of sum a and b
     """
 
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError("a must be an integer")
     if not isinstance(b, int) and not isinstance(b, float):
         raise TypeError("b must be an integer")
-    return int(a + b)
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")

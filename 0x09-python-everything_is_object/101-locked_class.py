09d3bae1aeb741f4a8e6459eb1b8abb30f15dd76
Mrelshimy <mraafat.elsayed@gmail.com>
2023-11-28 06:11:30 -0800

    update

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,8 +1,11 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 """Locked Class Module"""
 
 
 class LockedClass:
-    """Class with locked attribute"""
+    """
+    Class with locked attribute to prevent creation
+    of any attribute with name other than "first_name"
+    """
 
     __slots__ = ["first_name"]

ece8ecc6f04dc09239cbd37343f6993ea799f401
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-04 02:57:41 -0800

    update

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Check object is exactly an instance of class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """compare object and class instance"""
+
+    return isinstance(obj, a_class)

887e301f9a0ced6bbd49bddbfd3540429522dbb2
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-04 01:40:50 -0800

    update

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!usr/bin/python3
+""" Module to returns the list of attributes and methods of object"""
+
+
+def lookup(obj):
+    """Function to return object __dict___"""
+
+    return dir(obj)
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,12 @@
+#!usr/bin/python3
+"""MyList Class"""
+
+
+class MyList(list):
+    """List class"""
+
+    def print_sorted(self):
+        """return a sorted list"""
+
+        a = self[:]
+        print(sorted(a))

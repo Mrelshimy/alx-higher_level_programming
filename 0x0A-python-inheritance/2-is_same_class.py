6adbb97ff196138a7dd9a4908d28f9216c98f3e2
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-04 02:51:58 -0800

    update

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Check object is exactly an instance of class"""
+
+
+def is_same_class(obj, a_class):
+    """compare object and class instance"""
+
+    return type(obj) == a_class

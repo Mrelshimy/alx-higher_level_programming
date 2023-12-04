5d0bf0a3268ce456d5f6665fedfbd4876a0f3b00
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-04 05:23:34 -0800

    update

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Rectangle class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Square class"""
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """area calculation method"""
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__size) + "/" + str(self.__size)

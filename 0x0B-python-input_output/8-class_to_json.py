f0db0fe150605016163d0698de58aa03726f92bc
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-05 11:32:46 -0800

    update

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" Function to add all args to list and save to file"""
+
+
+def class_to_json(obj):
+    """Return dictionary discription of object data"""
+
+    return obj.__dict__

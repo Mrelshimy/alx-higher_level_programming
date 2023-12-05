ab6acf66439e7846c00641e04d3730071cb11974
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-05 03:09:35 -0800

    update

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,8 +2,8 @@
 """ Function to add all args to list and save to file"""
 import json
 import sys
-from "5-save_to_json_file" import save_to_json_file
-from "6-load_from_json_file" import load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 a = list(sys.argv[:])

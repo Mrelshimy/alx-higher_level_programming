1213a3418c869e57f264e16c7c7e5997c2c89b99
Mrelshimy <mraafat.elsayed@gmail.com>
2023-12-05 01:12:13 -0800

    update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,6 +6,6 @@ def read_file(filename=""):
     """Functioon to read file and print to stdout"""
 
     import sys
-    with open(filename, "r") as fp:
+    with open(filename, encoding="utf-8", "r") as fp:
         for line in fp:
             print(line, file=sys.stdout)

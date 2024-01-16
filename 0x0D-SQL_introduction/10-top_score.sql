67a3d747924451e6daae747aff0a05c759edc884
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-16 12:17:09 +0200

    update

--- 0x0D-SQL_introduction/10-top_score.sql
+++ 0x0D-SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- Script to list all records of the table second_table in the database 
-SELECT name, score FROM `second_table` ORDER BY score DESC;
+SELECT score, name FROM `second_table` ORDER BY score DESC;
--- 0x0D-SQL_introduction/14-average.sql
+++ 0x0D-SQL_introduction/14-average.sql
@@ -1,3 +1,3 @@
 -- Script to compute the score average of all records in the table second_table
 SELECT AVG(score) AS `average`
-FROM `second_table`
+FROM `second_table`;

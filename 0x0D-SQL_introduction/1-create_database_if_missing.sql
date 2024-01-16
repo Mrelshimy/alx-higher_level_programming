dc755ce2d8d38b17294e9a5b787ae8a6567e58d4
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-16 11:23:25 +0200

    updating files

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,2 +1,2 @@
 -- Script to creta database hbtn_0c_0 if not existing
-CREATE DATABASE hbtn_0c_0 IF NOT EXISTS;
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
--- 0x0D-SQL_introduction/2-remove_database.sql
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -1,2 +1,2 @@
 -- Script to delete database hbtn_0c_0 if existing
-DROP DATABASE hbtn_0c_0 IF EXISTS;
+DROP DATABASE IF EXISTS `hbtn_0c_0`;

1a8ea3011193dd9c9937faa787b53a558cde3523
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-17 20:48:22 +0200

    adding file 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,4 +3,4 @@ SELECT tv_genres.name AS 'genre', COUNT(tv_show_genres.show_id) AS 'number_of_sh
 FROM tv_genres
 JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
-ORDER BY number_of shows DESC;
+ORDER BY number_of_shows DESC;

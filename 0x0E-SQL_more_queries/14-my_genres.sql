f3265862cf849a45cd89df7b0ad8aeca57af70c8
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-17 21:04:55 +0200

    adding file 14

---
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -0,0 +1,7 @@
+--  Script that uses the hbtn_0d_tv shows database to lists all genres of the show Dexter
+SELECT name FROM tv_genres
+LEFT JOIN tv_shows_genres ON tv_genres.id = tv_shows_genres.genre_id
+LEFT JOIN tv_shows ON tv_shows.id = tv_shows_genres.show_id
+WHERE tv_shows.title = 'Dexter'
+GROUP BY name
+ORDER BY name ASC;

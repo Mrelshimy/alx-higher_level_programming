57dec7cfd763ab4d71c1e83aa3ad22acabe1802f
Mrelshimy <mraafat.elsayed@gmail.com>
2024-01-17 01:23:57 -0800

    adding files

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
--- Script that creates the table unique_id on your MySQL server.
+-- Script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa).
 CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa `;
 USE `hbtn_0d_usa`;
-CREATE TABLE IF NOT EXISTS `states `(
+CREATE TABLE IF NOT EXISTS `states`(
 	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
 	name VARCHAR(256) NOT NULL
 );
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- Script that reates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) .
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa `;
+USE `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `cities`(
+	id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(256) NOT NULL,
+	state_id INT NOT NULL FOREIGN KEY REFERENCES states(id)
+);

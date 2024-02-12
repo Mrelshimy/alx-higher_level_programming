86d89898ed063aac9c7efd0bd6c12e3c5aad6d62
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-12 20:05:38 +0200

    update == to ===

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,7 +2,7 @@
 if (process.argv.length <= 2) {
     console.log('No argument');
 }
-else if (process.argv.length == 3) {
+else if (process.argv.length === 3) {
     console.log('Argument found');
 }
 else {

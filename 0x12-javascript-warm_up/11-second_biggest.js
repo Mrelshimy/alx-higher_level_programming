6f1f9ceebcca970ee18ef211351c7b74baf057f5
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-12 20:51:39 +0200

    Bug fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,19 @@
 #!/usr/bin/node
+let flag = 0;
 if (process.argv.length < 4) {
   console.log(0);
 } else {
   process.argv.sort();
   process.argv.reverse();
-  console.log(process.argv[1]);
+  for (let i = 2; i < process.argv.length; i++) {
+    if (process.argv[i] > 0) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag === 1) {
+    console.log(process.argv[1]);
+  } else {
+    console.log(process.argv[process.argv.length - 2]);
+  }
 }

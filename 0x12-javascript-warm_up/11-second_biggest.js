91f09dce5317a62e43db286e1ec7810c6fa89fa0
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-12 20:59:49 +0200

    bugfix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-let flag = 0;
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  process.argv.sort();
+  process.argv.sort((a, b) => a - b);
   process.argv.reverse();
-  for (let i = 2; i < process.argv.length; i++) {
-    if (process.argv[i] > 0) {
-      flag = 1;
-      break;
-    }
-  }
-  if (flag === 1) {
-    console.log(process.argv[1]);
-  } else {
-    console.log(process.argv[process.argv.length - 2]);
-  }
+  console.log(process.argv);
 }

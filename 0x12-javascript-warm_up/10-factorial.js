b52a0b16f7d1072b3ea6f44a267a09731ae1c5e3
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-12 21:28:07 +0200

    bigfix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,7 +1,11 @@
 #!/usr/bin/node
+const a = parseInt(process.argv[2]);
 function factorial (a) {
-  if (parseInt(a) > 0) {
-    return (a * factorial(parseInt(a - 1)));
+  if (a > 0) {
+    return a * factorial(a - 1);
+  } else {
+    return 1;
   }
 }
-console.log(factorial(process.argv[2]));
+const facResult = factorial(a);
+console.log(facResult);

2d8ea971dcd7f70a7616280bd05e349bef19a82a
Mrelshimy <mraafat.elsayed@gmail.com>
2024-02-29 21:22:50 +0200

    add file

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that displays only body of a 200 status code response
-curl -sI "$1" | grep "ALLOW" | cut -d " " -f 2-
+curl -sX OPTIONS -I "$1" | grep -i "Allow:" | cut -d ' ' -f 2-

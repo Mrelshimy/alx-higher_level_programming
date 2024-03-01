9dc971810c66ea9def5034cbeaf1b13a7633a6b0
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-01 19:28:57 +0200

    update

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,11 +5,11 @@ if __name__ == "__main__":
     import requests
     import sys
 
-    if not sys.argv[2]:
+    if not sys.argv[1]:
         attr = ""
     else:
-        attr = sys.argv[2]
-        responce = requests.post(sys.argv[1], data={'q', attr})
+        attr = sys.argv[1]
+        responce = requests.post("http://0.0.0.0:5000/search_user", data={'q': attr})
         resp_json = responce.json()
         try:
             if resp_json:

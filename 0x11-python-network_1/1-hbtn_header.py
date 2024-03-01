de92f5508e89785c9f6089c3337514e992f29199
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-01 16:39:54 +0200

    Add file

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    from urllib import request
+    import sys
+
+    with request.urlopen(sys.argv[1]) as res:
+        print(dict(res.headers).get('X-Request-Id'))

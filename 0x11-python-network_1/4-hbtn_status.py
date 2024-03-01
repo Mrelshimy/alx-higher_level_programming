83d5fe0eac7c47dee32f4f5710752affb5025c14
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-01 18:36:20 +0200

    update

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -3,7 +3,7 @@
 
 if __name__ == "__main__":
     import requests
-    
+
     responce = requests.get('https://alx-intranet.hbtn.io/status')
     print('Body response:')
     print(f'\t- type: {type(responce.text)}')

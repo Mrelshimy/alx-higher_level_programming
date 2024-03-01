0679143ad53c5ccb45d0b1f4a2b7d44272e5248f
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-01 19:18:36 +0200

    update

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Python script that fetches https://alx-intranet.hbtn.io/status"""
+
+if __name__ == "__main__":
+    import requests
+    import sys
+
+    if sys.argv[2]:
+        responce = requests.post(sys.argv[1], data={'q', sys.argv[2]})
+        resp_json = responce.json()
+        try:
+            if not resp_json or resp_json == {}:
+                print("Not a valid JSON")
+            else:
+                print(f'[{resp_json.get("id")}] {resp_json.get("name")}')
+        except ValueError:
+            print("Not a valid JSON")
+    else:
+        print("No result")

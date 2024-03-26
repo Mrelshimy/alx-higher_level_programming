5647eec83fa0a8b505b78a033087117189d66212
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-26 22:12:51 +0200

    update

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,18 +7,15 @@ request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err
     console.log(err);
   } else {
     const data = JSON.parse(body);
-    const results = data.results;
-    for (const result of results) {
-      for (const character of result.characters) {
-        request.get(character, (error, responce, data) => {
-          if (error) {
-            console.error(error);
-          } else {
-            data = JSON.parse(data);
-            console.log(data.name);
-          }
-        });
-      }
+    for (const character of data.characters) {
+      request.get(character, (error, responce, newData) => {
+        if (error) {
+          console.error(error);
+        } else {
+          const charData = JSON.parse(newData);
+          console.log(charData.name);
+        }
+      });
     }
   }
 }

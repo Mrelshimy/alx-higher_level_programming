d4bba130b37c8d3e23593fcc2a2628cbbb82f5b1
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-26 21:53:06 +0200

    update

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], 'utf8', (err, response, body) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  const compTasks = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!compTasks[todo.userId]) {
+        compTasks[todo.userId] = 1;
+      } else {
+        compTasks[todo.userId] += 1;
+      }
+    }
+  });
+  console.log(compTasks);
+});

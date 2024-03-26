9b63ebcb8eb27f9c1fd36690c7b41d50b4cd2cfb
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-26 21:17:54 +0200

    update

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -7,6 +7,6 @@ request.get(`https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`, (err
     console.log(err);
   } else {
     const episode = body.JSONparse();
-    console.log(`${episode.title}`);
+    console.log(episode.title);
   }
 });

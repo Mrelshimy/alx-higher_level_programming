ef6b3d4b47fb7a3c8e5bf2a9a915d70dd57c7028
Mrelshimy <mraafat.elsayed@gmail.com>
2024-03-26 19:58:02 +0200

    update

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+request.get(process.argv[2], (err, responce) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`code: ${responce}`);
+  }
+});
